b987fb1328e647b3ec40fc93911a355472e4d811
ANIL A S <134312212+anil-as@users.noreply.github.com>
2025-03-12 16:55:18 +0530

    Update missing_number.py

--- missing_number.py
+++ missing_number.py
@@ -10,3 +10,13 @@ If the array contains the numbers: [1, 2, 3, 5, 6]
 Then the missing number is 4.
 
 """
+array = [1, 2, 3, 4, 5]
+n = 6 
+for i in range(1, n + 1):  
+    count = 0
+    for j in array: 
+        if i == j:
+            count += 1 
+    if count == 0:
+        print("Missing element is:", i)
+        break 
